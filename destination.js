5b287fb52f6ea756eec123b94c5797bdf0f1ea87
Lucas <lkwalton@uncg.edu>
2026-01-29 13:27:11 -0500

    added finall touches

--- destination.js
+++ destination.js
@@ -4,8 +4,3 @@ homeButton.addEventListener('click', function () {
   window.location.href = 'index.html';
 });
 
-let congoButton = document.getElementById('congo-button');
-
-congoButton.addEventListener('click', function () {
-  window.location.href = 'destination.html';
-});
